ff24d078eb799e9e5d3ae827a6385723c5466712
Dariusz Okonski <darek200180@gmail.com>
2019-10-20 21:12:47 +0100

    add basic variables and handlers

---
+++ app.js
@@ -0,0 +1,14 @@
+let min = 1;
+let max = 10;
+let winningNum = 2;
+let guessesLeft = 3;
+
+const game = document.querySelector('#game');
+const minNum = document.querySelector('.min-num');
+const maxNum = document.querySelector('.max-num');
+const guessBtn = document.querySelector('#guess-btn');
+const guessInput = document.querySelector('#guess-input');
+const message = document.querySelector('.message');
+
+minNum.textContent = min;
+maxNum.textContent = max;
